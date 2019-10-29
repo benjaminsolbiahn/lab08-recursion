31788bf385a08c22d84cc91f25eb8ab77df147ed
Paul Inventado <pinventado@gmail.com>
2019-10-29 15:28:22 -0700

    Initial commit

---
+++ prob04/main.cpp
@@ -0,0 +1,20 @@
+#include "gcd.hpp"
+#include <iostream>
+
+int main() {
+  int num_1;
+  int num_2;
+  std::cout << "Enter the first number: ";
+  std::cin >> num_1;
+  std::cout << "Enter the second number: ";
+  std::cin >> num_2;
+
+  int gcd_value;
+  // Provide code that calls the gcd function using the two numbers that the
+  // user entered. Store the result in the gcd_value variable.
+
+  std::cout << "The GCD of the numbers " << num_1 << " and " << num_2 << " is "
+            << gcd_value << "\n";
+
+  return 0;
+}
---
+++ prob05/main.cpp
@@ -0,0 +1,12 @@
+#include "array_max.hpp"
+#include <iostream>
+
+int main() {
+  int arr[10] = {10, 9, 6, 1, 2, 4, 16, 8, 7, 5};
+  int max_value;
+  // Provide code that calls the array_max function and passes the array and its size
+  // as arguments. Store the result in the variable called max_value
+
+  std::cout << "The largest number in the array is " << max_value << "\n";
+  return 0;
+}
